f585f2231b99ced4256a4bb8bb6833d881aee836
Showsan <jester859@gmail.com>
2025-07-22 08:24:27 -0500

    Save 2
    I've started a new save to make my ideas easier to write. These will automate most everything with minimal changes needed.

--- helpers.py
+++ helpers.py
@@ -1,3 +1,20 @@
+item_caps = {
+    "hay_cap": 1000000,
+    "wood_cap": 1000000,
+    "carrot_cap": 1000000,
+    "pumpkin_cap": 1000000,    
+    "bone_cap": 1000000,
+    "cactus_cap": 1000000,
+    "power_cap": 50000,
+    "gold_cap": 100000,
+    "fert_min" : 10000
+}
+
+x = get_pos_x()
+y = get_pos_y()
+
+ws = get_world_size()
+
 def move_to(target_x, target_y):
     while get_pos_x() != target_x:
         if (target_x - get_pos_x()) % get_world_size() > get_world_size() / 2:
@@ -10,5 +27,14 @@ def move_to(target_x, target_y):
         else:
             move(North)
 
+def soil():
+    if get_ground_type() != Grounds.Soil:
+        till()
 
-ws = get_world_size()
+def water():
+    if get_water() < 0.3:
+        use_item(Items.Water)
+
+def harvest_now():
+    if can_harvest():
+        harvest()
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+from helpers import item_caps
 import farm
-import unlocks
-import helpers
 
-while True:
-    farm.poly()
-    farm.pumpkin()
-    farm.cactus()
-    farm.dino()
-    farm.sunflower()
-    farm.maze_starter()
-    do_a_flip()
-    break
+while True:	
+	if num_items(Items.Carrot) < item_caps["carrot_cap"]:
+		farm.poly()
+	elif num_items(Items.Pumpkin) < item_caps["pumpkin_cap"]:
+		farm.pumpkin_harvest()
+	elif num_items(Items.Bone) < item_caps["bone_cap"]:
+		farm.dino()
+	elif num_items(Items.Cactus) < item_caps["cactus_cap"]:
+		farm.cactus()
+	elif num_items(Items.Power) < item_caps["power_cap"] and num_items(Items.Fertilizer) > item_caps["fert_min"]:	
+		farm.sunflower()
+	elif num_items(Items.Gold) < item_caps["gold_cap"]:
+		farm.maze_starter()
+	else:
+		clear()		
+		do_a_flip()
+		print("Done Massa!")
