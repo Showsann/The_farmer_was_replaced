1b7e219ec6d47294552cdf6b889d8fe772e6cec9
Showsan <jester859@gmail.com>
2025-07-23 13:28:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from helpers import item_caps
+from helpers import (
+	item_caps,
+	target_items,
+	try_unlock
+)
 import farm
 
-while True:	
-	if num_items(Items.Carrot) < item_caps["carrot_cap"]:
-		farm.poly()
-	elif num_items(Items.Pumpkin) < item_caps["pumpkin_cap"]:
-		farm.pumpkin_harvest()
-	elif num_items(Items.Bone) < item_caps["bone_cap"]:
-		farm.dino()
-	elif num_items(Items.Cactus) < item_caps["cactus_cap"]:
-		farm.cactus()
-	elif num_items(Items.Power) < item_caps["power_cap"] and num_items(Items.Fertilizer) > item_caps["fert_min"]:	
-		farm.sunflower()
-	elif num_items(Items.Gold) < item_caps["gold_cap"]:
-		farm.maze_starter()
-	else:
-		clear()		
+while True:
+	if try_unlock():
+		continue
+
+	did_refill = False
+	for item, cap, handler in target_items:
+		if num_items(item) < cap:
+			handler()
+			did_refill = True				
+			break
+
+	if not did_refill:
+		clear()
 		do_a_flip()
-		print("Done Massa!")
